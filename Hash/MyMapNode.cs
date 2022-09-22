39c403215c871e28fbb32b0939ac705ae2417176
kavyabs11 <kavyasomshekar11@gmail.com>
2022-09-22 07:42:52 -0700

    Removing Of Words

--- Hash/MyMapNode.cs
+++ Hash/MyMapNode.cs
@@ -9,26 +9,17 @@ namespace Hash
     public class MyMapNode<K, V>
     {
         internal int Size;
-
-        //Create an array of LinkedList of KeyValue pair containing K, V as key and value of name items
         internal LinkedList<KeyValue<K, V>>[] Items;
 
         public MyMapNode(int Size)
         {
             this.Size = Size;
-
-            //size of array of LinkedList is passed while creating objects in main program
             Items = new LinkedList<KeyValue<K, V>>[Size];
         }
 
-        //to get the position of the key in the array by formula given
         public int ArrayPosition(K key)
         {
-            //to resolve conflicts in array , we use formula to calculate position
-            //position = (hash code of the key) % (size of the array)
             int position = key.GetHashCode() % Size;
-
-            //returning the positive value of position
             return Math.Abs(position);
         }
 
@@ -37,37 +28,28 @@ namespace Hash
             int position = ArrayPosition(key);
             LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
 
-            //checking for each KeyValue pair in our linked list
             foreach (KeyValue<K, V> Items in linkedlist)
             {
-                //if our Key in item is matching with the key or not
+
                 if (Items.Key.Equals(key))
                 {
-                    //return the value present in item , i.e. value situated at the position
                     return Items.Value;
                 }
             }
             return default(V);
         }
-
-        //For adding items in our LinkedList
         public void Add(K key, V value)
         {
-            //get position of our item using key
             int position = ArrayPosition(key);
 
             LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
             KeyValue<K, V> Items = new KeyValue<K, V>() { Key = key, Value = value };
 
-            //add that item at the last position/ at the end of the linkedlist
             linkedlist.AddLast(Items);
         }
 
-        //creatig GetLinkedList method having return type as LinkedList
-        //each value in LinkedList is containing KeyValue 
         public LinkedList<KeyValue<K, V>> GetLinkedList(int position)
         {
-            //passing the position of the item to the LinkedList named linkedlist
             LinkedList<KeyValue<K, V>> linkedlist = Items[position];
 
             if (linkedlist == null)
@@ -78,13 +60,33 @@ namespace Hash
             return linkedlist;
         }
 
-        //Creating a structure(struct) of type KeyValue
-        //struct is used to group many related variables into one place
-        // collection of many different datatypes
         public struct KeyValue<k, v>
         {
             public k Key { get; set; }
             public v Value { get; set; }
+        }
+
+        public void Remove(K key)
+        {
+            int position = ArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+
+            bool ItemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                if (item.Key.Equals(key))
+                {
+                    ItemFound = true;
+                    foundItem = item;
+                }
+            }
+
+            if (ItemFound)
+            {
+                linkedlist.Remove(foundItem);
+            }
         }
     }
 }
--- Hash/Program.cs
+++ Hash/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Hash;
+﻿using Hash;
 using Hash;
 Console.WriteLine("Welcome to HashTables");
 
@@ -8,6 +7,8 @@ while (true)
     Console.WriteLine("\n0. Exit");
     Console.WriteLine("1.Find frequency of words in a sentence");
     Console.WriteLine("Q2. Find Frequency of Words in a Large Paragraph Phrase");
+    Console.WriteLine("Q3. Remove Avoidable Word From Phrase");
+
 
     Console.Write("\nEnter Question Number You Want to Execute : ");
     int Choice = Convert.ToInt32(Console.ReadLine()); // Taking Input From User
@@ -15,13 +16,17 @@ while (true)
     {
         case 1:
             string frequency = "To be or not to be";
-            List<string> compareList = new List<string>();
-            List<string> storeCountList = new List<string>();
-            string[] frequencyArray = frequency.Split(' ');
-            MyMapNode<int, string> hash1 = new MyMapNode<int, string>(frequencyArray.Length);
 
+            List<string> compareList = new List<string>();
+
+            List<string> storeCountList = new List<string>();
+
+            string[] frequencyArray = frequency.Split(' ');
+
+            MyMapNode<int, string> hash1 = new MyMapNode<int, string>(frequencyArray.Length);
             for (int i = 0; i < frequencyArray.Length; i++)
             {
+
                 hash1.Add(i, frequencyArray[i]);
                 Console.Write(hash1.Get(i) + " ");
             }
@@ -33,6 +38,7 @@ while (true)
                 {
                     if (frequencyArray[j].ToLower() == hash1.Get(i).ToLower())
                     {
+
                         count++;
 
                         if (compareList.Contains(hash1.Get(j).ToLower()))
@@ -49,10 +55,8 @@ while (true)
 
                 compareList.Add(hash1.Get(i));
 
-
                 storeCountList.Add(hash1.Get(i) + "\t" + count);
             }
-
 
             Console.WriteLine("Word and their frequencies shown below:- ");
             for (int i = 0; i < storeCountList.Count; i++)
@@ -68,19 +72,15 @@ while (true)
 
             List<string> compareList2 = new List<string>();
 
-
             List<string> storeCountList2 = new List<string>();
 
-
             string[] frequencyArray2 = frequency_2.Split(' ');
-
 
             MyMapNode<int, string> hash2 = new MyMapNode<int, string>(frequencyArray2.Length);
 
             Console.WriteLine("Given phrase is :\n");
             for (int i = 0; i < frequencyArray2.Length; i++)
             {
-
                 hash2.Add(i, frequencyArray2[i]);
                 Console.Write(hash2.Get(i) + " ");
             }
@@ -94,32 +94,53 @@ while (true)
 
                     if (frequencyArray2[j].ToLower() == hash2.Get(i).ToLower())
                     {
-                        count++;
 
+                        count++;
                         if (compareList2.Contains(hash2.Get(j).ToLower()))
                         {
                             break;
                         }
                     }
                 }
-
                 if (compareList2.Contains(hash2.Get(i).ToLower()))
                 {
                     continue;
                 }
-
                 compareList2.Add(hash2.Get(i));
-
 
                 storeCountList2.Add(hash2.Get(i) + "\t\t" + count);
             }
-
             Console.WriteLine("\nWord and their frequencies shown below :- ");
             for (int i = 0; i < storeCountList2.Count; i++)
             {
                 Console.WriteLine(storeCountList2[i]);
             }
             Console.WriteLine("##################################################################");
+            break;
+
+        case 3:
+            string removeString = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+
+            string[] removeStringArray = removeString.Split(' ');
+
+            MyMapNode<int, string> hash3 = new MyMapNode<int, string>(removeStringArray.Length);
+            Console.WriteLine("Given phrase is :- \n");
+
+            for (int i = 0; i < removeStringArray.Length; i++)
+            {
+                hash3.Add(i, removeStringArray[i]);
+                Console.Write(hash3.Get(i) + " ");
+            }
+
+            hash3.Remove(17);
+
+            Console.WriteLine("\n\nAfter Removing Word \"Avoidable\" from Phrase, the phrase is :-");
+
+            for (int i = 0; i < removeStringArray.Length; i++)
+            {
+                Console.Write(hash3.Get(i) + " ");
+            }
+            Console.WriteLine("\n#################################################");
             break;
 
         default:
