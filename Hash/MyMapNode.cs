54abba993ace6a394893302708f41f1bd5b278bd
kavyabs11 <kavyasomshekar11@gmail.com>
2022-09-22 07:27:27 -0700

    Finding frequency Of words

---
+++ Hash/MyMapNode.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash
+{
+    public class MyMapNode<K, V>
+    {
+        internal int Size;
+
+
+        internal LinkedList<KeyValue<K, V>>[] Items;
+
+        public MyMapNode(int Size)
+        {
+            this.Size = Size;
+
+
+            Items = new LinkedList<KeyValue<K, V>>[Size];
+        }
+
+
+        public int ArrayPosition(K key)
+        {
+            //to resolve conflicts in array , we use formula to calculate position
+            //position = (hash code of the key) % (size of the array)
+            int position = key.GetHashCode() % Size;
+
+            //returning the positive value of position
+            return Math.Abs(position);
+        }
+
+        public V Get(K key)
+        {
+            int position = ArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+
+
+            foreach (KeyValue<K, V> Items in linkedlist)
+            {
+
+                if (Items.Key.Equals(key))
+                {
+
+                    return Items.Value;
+                }
+            }
+            return default(V);
+        }
+
+
+        public void Add(K key, V value)
+        {
+            //get position of our item using key
+            int position = ArrayPosition(key);
+
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            KeyValue<K, V> Items = new KeyValue<K, V>() { Key = key, Value = value };
+
+
+            linkedlist.AddLast(Items);
+        }
+
+        public LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedlist = Items[position];
+
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                Items[position] = linkedlist;
+            }
+            return linkedlist;
+        }
+        public struct KeyValue<k, v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+    }
+}
--- Hash/Program.cs
+++ Hash/Program.cs
@@ -1,10 +1,86 @@
-﻿namespace Hash
+﻿// See https://aka.ms/new-console-template for more information
+using Hash;
+Console.WriteLine("Welcome to HashTables");
+
+while (true)
 {
-    internal class Program
+    Console.WriteLine("\n0. Exit");
+    Console.WriteLine("1.Find frequency of words in a sentence");
+
+    Console.Write("\nEnter Question Number You Want to Execute : ");
+    int Choice = Convert.ToInt32(Console.ReadLine()); // Taking Input From User
+    switch (Choice)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        case 1:
+            //Declaring String
+            string frequency = "To be or not to be";
+
+            //Declaring List to Comare With Hash Table
+            List<string> compareList = new List<string>();
+
+            //Declaring List to Store Words 
+            List<string> storeCountList = new List<string>();
+
+            //Spliting the String store in Array
+            string[] frequencyArray = frequency.Split(' ');
+
+            //Declaring Length of Frequency Array
+            MyMapNode<int, string> hash1 = new MyMapNode<int, string>(frequencyArray.Length);
+            for (int i = 0; i < frequencyArray.Length; i++)
+            {
+                //Adding Words in Hast Table
+                hash1.Add(i, frequencyArray[i]);
+
+                //Displaying Word & Store in Hash Table
+                Console.Write(hash1.Get(i) + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < frequencyArray.Length; i++)
+            {
+                //Declaring count to count number of occurances of the words
+                int count = 0;
+                for (int j = 0; j < frequencyArray.Length; j++)
+                {
+                    //If our phrase array contains same word
+                    if (frequencyArray[j].ToLower() == hash1.Get(i).ToLower())
+                    {
+                        //then we will increase the count
+                        count++;
+
+                        //Condition checking and loop breaking for internal loop
+                        //to avoid duplicate entries in our table we are using a condition
+                        if (compareList.Contains(hash1.Get(j).ToLower()))
+                        {
+                            //if value is already available in our compareList2 list then we'll break internal loop
+                            break;
+                        }
+                    }
+                }
+                //Condition checking and loop breaking for external loop
+                //if value already present in our compareList2 list then we'll continue from next iteration
+                if (compareList.Contains(hash1.Get(i).ToLower()))
+                {
+                    continue;
+                }
+                //Adding the word to compareList for further comparison for duplicates
+                compareList.Add(hash1.Get(i));
+
+                //finally storing the word and its frequency in storeCountList list
+                storeCountList.Add(hash1.Get(i) + "\t" + count);
+            }
+
+            //Printing each word in our sentence and its frequency stored in storeCountList
+            Console.WriteLine("Word and their frequencies shown below:- ");
+            for (int i = 0; i < storeCountList.Count; i++)
+            {
+                Console.WriteLine(storeCountList[i]);
+            }
+            break;
+        default:
+            Console.WriteLine("Please enter correct Question Number");
+            break;
+        case 0:
+            return;
     }
 }
+Console.ReadKey();
